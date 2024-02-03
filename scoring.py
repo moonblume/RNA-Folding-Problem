e8f3d99996bc0da8e2e827cd4d42d2de3ea541f6
Clara <126781673+moonblume@users.noreply.github.com>
2024-02-03 16:58:35 +0100

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-bins = np.arange(0, 20)+1
+bins = np.arange(0, 20) + 1
 interpolation_function = np.interp(AA, bins, pseudo_energy[0:20])
-interpolation_function
 
-pseudo_energy[0:20]
+print("Interpolation Function:", interpolation_function)
 
+pseudo_energy_slice = pseudo_energy[0:20]
+print("Pseudo Energy (0-19):", pseudo_energy_slice)
