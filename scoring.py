a1b5ab66c1ad8522fdb4d10e0192f9b02ad5f164
Clara <126781673+moonblume@users.noreply.github.com>
2024-02-03 22:02:41 +0100

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,15 +1,18 @@
 import numpy as np
 from training import calculate_pseudo_energy, AA
 
-bins = np.arange(0, 20) + 1
-
 # Call the calculate_pseudo_energy function to get the pseudo_energy data
 pseudo_energy = calculate_pseudo_energy()
 
-# Perform interpolation using AA and pseudo_energy
-interpolation_function = np.interp(AA, bins, pseudo_energy[0:20])
+bins = np.arange(0, 20) + 1
 
-print("Interpolation Function:", interpolation_function)
+interpolation_function = np.interp(AA, bins, pseudo_energy[0:20]) #interpolation function based on pseudo-energy of AA pair
 
-pseudo_energy_slice = pseudo_energy[0:20]
-print("Pseudo Energy (0-19):", pseudo_energy_slice)
+print("Distances in AA:", AA)
+print("Pseudo-energy of AA for bins:", pseudo_energy[0:20])
+print("Results of interpolation:", interpolation_function)
+#we can see that the pseudo-energy per each distance in AA list is approximately the same as pseudo-energy according to the bin, to which this particular distance belongs
+#the same thing we can do with each nucleotides pair and their pseudo energy
+
+
+
