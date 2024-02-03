587cf7dd8d0c598250cb0281d88c63e203f3b9ef
Clara <126781673+moonblume@users.noreply.github.com>
2024-02-03 17:10:06 +0100

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,6 +1,11 @@
-import numpy as np
+from training import calculate_pseudo_energy, AA
 
 bins = np.arange(0, 20) + 1
+
+# Call the calculate_pseudo_energy function to get the pseudo_energy data
+pseudo_energy = calculate_pseudo_energy()
+
+# Perform interpolation using AA and pseudo_energy
 interpolation_function = np.interp(AA, bins, pseudo_energy[0:20])
 
 print("Interpolation Function:", interpolation_function)
